2a86787fe9a0323bce8ea5e8f011f852cd573f85
lunaticwan <3921422+lunaticwan@users.noreply.github.com>
2026-09-08 13:05:05 +0000

    feat: update fonts to Pretendard and Cascadia Code, set default language to Korean

--- src/marble.ts
+++ src/marble.ts
@@ -180,7 +180,7 @@ export class Marble {
 
   private _drawName(ctx: CanvasRenderingContext2D, zoom: number) {
     transformGuard(ctx, () => {
-      ctx.font = `12pt sans-serif`;
+      ctx.font = `12pt Pretendard, sans-serif`;
       ctx.strokeStyle = 'black';
       ctx.lineWidth = 2;
       ctx.fillStyle = this.color;
